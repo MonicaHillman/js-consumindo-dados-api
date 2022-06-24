f1748a948e025582c47650695a489225624c9d61
MonicaHillman <mmhillman1@ucs.br>
2022-06-24 14:52:28 -0300

    lidando com requisições com async await

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-    .then(resposta => resposta.json())
-    .then(r => {
-        if (r.erro) {
-            throw Error('Esse cep não existe!')
-        } else
-            console.log(r)
-    })
-    .catch(erro => console.log(erro))
-    .finally(mensagem => console.log('Processamento concluido!'));
+async function buscaEndereco(cep) {
+    try {
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var consultaCEPConvertida = await consultaCEP.json();
+        if (consultaCEPConvertida.erro) {
+            throw Error('CEP não existente!');
+        }
+        console.log(consultaCEPConvertida);
+        return consultaCEPConvertida;
+    } catch (erro) {
+        console.log(erro);
+    }
+}
 
-console.log(consultaCEP);
+/* --- Lidando com várias requisições ao mesmo tempo com Promise.all ---
+
+let ceps = ['01001000', '01001001'];
+let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
+console.log(conjuntoCeps);
+Promise.all(conjuntoCeps).then(respostas => console.log(respostas));
+
+*/
