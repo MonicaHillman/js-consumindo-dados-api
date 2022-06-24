0864afdb04e56cd60d9844bce7fbdceccfafd6d4
MonicaHillman <mmhillman1@ucs.br>
2022-06-24 14:39:05 -0300

    iniciando com fetch api

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-/*
+var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/');
 
---- Código da conversa versão síncrono ---
-
-console.log("Mandando oi pro amigo!");
-
-function mandaMensagem() {
-    console.log("Tudo bem?");
-    console.log("Vou te mandar uma solicitação!");
-    console.log("Solicitação recebida!");
-}
-
-mandaMensagem();
-
-console.log("Tchau tchau!");
-
-*/
-
-console.log("Mandando oi pro amigo!");
-
-function mandaMensagem() {
-    console.log("Tudo bem?");
-    console.log("Vou te mandar uma solicitação!");
-    console.log("Solicitação recebida!");
-}
-
-setTimeout(mandaMensagem, 5000);
-
-console.log("Tchau tchau!");
+console.log(consultaCEP);
