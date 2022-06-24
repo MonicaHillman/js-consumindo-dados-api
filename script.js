fd8977ea7ea711b489f34834e66db01d1f2ac949
MonicaHillman <mmhillman1@ucs.br>
2022-06-24 14:43:53 -0300

    lidando com promises através do método then, catch e finally

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/');
+var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
+    .then(resposta => resposta.json())
+    .then(r => {
+        if (r.erro) {
+            throw Error('Esse cep não existe!')
+        } else
+            console.log(r)
+    })
+    .catch(erro => console.log(erro))
+    .finally(mensagem => console.log('Processamento concluido!'));
 
 console.log(consultaCEP);
